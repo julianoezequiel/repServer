e367fff0481273e9dd2b79c2a7af8e85f5d1997c
julianoezequiel@gmail.com <julianoezequiel@gmail.com>
2017-03-21 08:06:58 -0300

    deploy Heroku

--- src/main/java/com/api/TomcatConfig.java
+++ src/main/java/com/api/TomcatConfig.java
@@ -17,7 +17,7 @@ import org.springframework.context.annotation.Configuration;
  * @author juliano.ezequiel
  *
  */
-@Configuration
+//@Configuration
 public class TomcatConfig {
 
 	@Value("${http.port}")
