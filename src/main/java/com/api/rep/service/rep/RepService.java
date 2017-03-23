d34131d4871658c9655a4a04359de060b1f40875
julianoezequiel@gmail.com <julianoezequiel@gmail.com>
2017-03-23 08:30:15 -0300

    heroku

--- src/main/java/com/api/rep/service/rep/RepService.java
+++ src/main/java/com/api/rep/service/rep/RepService.java
@@ -1,7 +1,5 @@
 package com.api.rep.service.rep;
 
-import static org.assertj.core.api.Assertions.in;
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -10,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import com.api.rep.contantes.CONSTANTES;
 import com.api.rep.dao.ConfiguracoesRedeRepository;
 import com.api.rep.dao.InfoRepository;
 import com.api.rep.dao.RepRepository;
